351272ee62678488f87414fbe85d1219344eafcd
agless <agless@users.noreply.github.com>
2017-04-23 10:06:07 -0400

    Comments
    Commented each line of main loop.

--- word_square.py
+++ word_square.py
@@ -25,68 +25,86 @@ while run == True:
 
     #make a list to hold partially consumed test words
     word_list_shelf = []
-    word_list_shelf.append('null')
+    word_list_shelf.append('null')  #seed word doesn't consume a list, so this is a placeholder
 
     #initialize other variables
     working_position = 1
-    square_solved = False
     test_strip = 'init'
     solutions = 0
 
+    #get the first fixed letter
     fixed_letters = get_fixed_letters(working_position, square_words)
-
+    #grab the first set of test words to cycle through
     word_list = word_lists[working_position].copy()
 
-    #Build the word square by recursively cycling through the lists
+    #Build word squares by recursively cycling through the lists
     while working_position < length:
         try:
-            test_word = word_list.pop()
-        except IndexError:
-            if working_position > 1:
-                working_position -= 1
-                word_list = word_list_shelf[working_position]
-                fixed_letters = get_fixed_letters(working_position, square_words)
-            elif working_position == 1:
-                if solutions == 0:
+            #grab a word
+            test_word = word_list.pop()  
+        #if there's no word to grab,
+        except IndexError:	
+            #if this isn't a final, fatal failure,
+            if working_position > 1:	
+                #back up a row.
+                working_position -= 1	
+                #grab the partially consumed word list from earlier
+                word_list = word_list_shelf[working_position]	
+                #get the fixed letters to check against
+                fixed_letters = get_fixed_letters(working_position, square_words)	
+            #if this is a final failure (since working_position 0 is the seed word)
+            elif working_position == 1:	
+                #if this is a total failure (no solutions found)
+                if solutions == 0:	
                     print('\nNo valid word_square exists.')
+                #otherwise, report the action taken
                 else:
-                    print('\nOperation Complete.')
-                working_position = length + 1
+                    print('\nOperation Complete.\nCheck the solutions folder.')
+                #break out of the recursive loop
+                working_position = length + 1	
         else:
-            test_strip = test_word[:working_position]
-            if test_strip == fixed_letters:
-                try:
+            #grab a slice the same length as current fixed letters to check against
+            test_strip = test_word[:working_position]	
+            #see if the slice matches the fixed letters
+            if test_strip == fixed_letters:	
+                try:	
+                    #maybe there's a solution word already in this spot
                     square_words[working_position] = test_word
-                except IndexError:
+                except IndexError:	
+                    #if not, make a new one
                     square_words.append(test_word)
                 try:
+                    #put the partially consumed list on the shelf, if a spot exists
                     word_list_shelf[working_position] = word_list
                 except IndexError:
+                    #if no spot exists, make a new one
                     word_list_shelf.append(word_list)
+                #move to the next row
                 working_position += 1
-                if working_position == length:
-                    #square_solved = True
+                #if this is a total solution (a valid word in the final row)
+                if working_position == length:	
+                    #increment the solutions count
                     solutions += 1
+                    #make a filename indicating the seed word
                     solution_filename = 'solutions/' + seed_word + '_word_squares.txt'
+                    #open the file in 'append mode' as an object to work with
                     with open(solution_filename, 'a') as solution_file:
+                        #make a loop that will run once for each solution word
                         for v in range(length):
+                            #write each solution word on its own line
                             solution_file.write(square_words[v].upper() + '\n')
+                        #write two blank lines after each complete solution for readability
                         solution_file.write('\n\n')
+                    #give feedback in the command line to show progress
                     print(str(solutions) + ' solution(s) found!!!')
+                    #drop back down so that remaining words in the list can be checked
                     working_position -= 1  
+                #if this isn't a total solution
                 else:
+                    #get new fixed letters to check against for this row
                     fixed_letters = get_fixed_letters(working_position, square_words)
+                    #get a fresh word list to cycle through for this position 
                     word_list = word_lists[working_position].copy()
-                        
-    #Print the word square
-    #All this will have to be changed for "find all"
-    #if square_solved == True:
-    #    print('\nSquare Solved!!!\n')
-    #    for z in range(length):
-    #        print(square_words[z].upper())
-
-    #Report action
-    print('Check the solutions folder.')
 
     #Ask to go again
     seed_word = input('\nEnter a new word?\n(Or "q" to quit):  ')
